58de5cc917de2645f33e2a54ff6cc8d09c6133b8
Tom de Grunt <tom@degrunt.nl>
2019-02-05 19:46:43 +0100

    Move backend setup stuff back to scribo

--- lib/liquor/engine.rb
+++ lib/liquor/engine.rb
@@ -6,8 +6,6 @@ module Liquor
     config.autoload_paths << File.expand_path(File.join(File.dirname(__FILE__), '.', 'liquid'))
 
     initializer 'liquor.config' do |_app|
-      # FIXME: How to make this work with Scribo and Nuntius
-      I18n.backend = I18n::Backend::Chain.new(Scribo::BucketI18nBackend.new, I18n.backend) if defined? I18n::HashRefinements
       path = File.expand_path(File.join(File.dirname(__FILE__), '.', 'liquid', '{tags,filters}', '*.rb'))
       Dir.glob(path).each do |c|
         require_dependency(c)

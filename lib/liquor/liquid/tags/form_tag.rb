5e2797618b98ef5ad1d28f86c020c18da31ed246
Tom de Grunt <tom@degrunt.nl>
2019-02-07 23:16:30 +0100

    Remove comments

--- lib/liquor/liquid/tags/form_tag.rb
+++ lib/liquor/liquid/tags/form_tag.rb
@@ -39,9 +39,6 @@ class FormTag < LiquorBlock
 
     if context.registers['controller']
       result += %[<input name="_method" type="hidden" value="#{rails_method}"/>] if rails_method
-
-      # embed_authenticity_token_in_remote_forms
-
       result += %[<input name="authenticity_token" type="hidden" value="#{context.registers['controller'].helpers.form_authenticity_token}"/>]
     end
 

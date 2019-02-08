06907af9ce9d8b36e17009fc6e1827e58ea9d7ee
Tom de Grunt <tom@degrunt.nl>
2019-02-08 14:24:51 +0100

    Improve options_for

--- lib/liquor/liquid/tags/options_for_select.rb
+++ lib/liquor/liquid/tags/options_for_select.rb
@@ -3,10 +3,15 @@
 # Add a text-field, either specifying everything manually or using a model object on the form
 #
 # == Basic usage:
-#    {%text_field name="name" value="Pencil"%}
+#    {%assign airports = "Eindhoven, Schiphol" | split: ', '%}
+#    {%options_for_select airports%}
 #
 # == Advanced usage:
-#    {%text_field name%}
+#    {%assign airports = "Eindhoven, Schiphol" | split: ', '%}
+#    {%options_for_select airports selected="Schiphol" disabled="Eindhoven"%}
+#
+# == Advanced usage:
+#    {%options_for_select airports name value selected="Schiphol" disabled="Eindhoven"%}
 #
 # This last usage requires a model on the form
 #
@@ -14,7 +19,17 @@ class OptionsForSelectTag < LiquorTag
   def render(context)
     super
 
-    context.registers['controller'].helpers.options_for_select(argv1).to_s
+    options = argv1.map(&:to_liquid)
+
+    if sargs.present?
+      options = options.map do |option|
+        result = sargs.map {|a| option[a.to_s] }
+        result = result.first if result.length == 1
+        result
+      end
+    end
+
+    context.registers['controller'].helpers.options_for_select(options, attr_args).to_s
   end
 end
 

20fa1d7bc8e1bd934a6052ec5f30db33badeb578
Tom de Grunt <tom@degrunt.nl>
2019-08-27 20:16:11 +0200

    Move errors to own tag

---
+++ lib/liquor/liquid/tags/field_error_tag.rb
@@ -0,0 +1,31 @@
+# frozen_string_literal: true
+
+# Add errors for a specific form field, only works inside a form
+#
+# == Basic usage:
+#    {%field_error name%}
+#
+class FieldErrorTag < LiquorTag
+  attr_accessor :field_type
+
+  def initialize(tag, args, tokens)
+    super
+    @field_type = 'text'
+  end
+
+  def render(context)
+    super
+
+    error_messages = context['form'].errors.messages[argv1]
+
+    if error_messages.present?
+      result += %[<span>] +
+        attr_str(:class, arg(:class), input(:class, argv1)) +
+        (error_messages || []).join(', ') + %[</span>]
+    end
+
+    result
+  end
+end
+
+Liquid::Template.register_tag('field_error', FieldErrorTag)

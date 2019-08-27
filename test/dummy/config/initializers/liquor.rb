72eb81bb1cf79e11b2d4efb3f9fbef0a8c11007b
Tom de Grunt <tom@degrunt.nl>
2019-08-27 10:45:21 +0000

    Fix tests

---
+++ test/dummy/config/initializers/liquor.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+Liquor.setup do |config| 
+
+end
--- test/liquid/translate_filter_test.rb
+++ test/liquid/translate_filter_test.rb
@@ -5,7 +5,7 @@ require 'test_helper'
 class TranslateFilterTest < ActiveSupport::TestCase
   test 'will allow translations' do
     I18n.stub(:t, 'smurrefluts') do
-      template_data = "{{'.dummy' | translate 'en'}}"
+      template_data = "{{'.dummy' | translate: locale: 'en'}}"
 
       template = Liquid::Template.parse(template_data)
       result   = template.render
@@ -15,7 +15,7 @@ class TranslateFilterTest < ActiveSupport::TestCase
   end
   test 'will allow translations, with short filter name' do
     I18n.stub(:t, 'smurrefluts') do
-      template_data = "{{'.dummy' | t: 'en'}}"
+      template_data = "{{'.dummy' | t: locale: 'en'}}"
 
       template = Liquid::Template.parse(template_data)
       result   = template.render

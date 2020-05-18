1384bff5524f13172d78b7fc8089ee43ede0cbbd
Mark Cornelissen <mark@boxture.com>
2020-05-18 12:46:21 +0200

    Default to [] if there are no invokable_methods

--- app/controllers/liquor/completions_controller.rb
+++ app/controllers/liquor/completions_controller.rb
@@ -3,10 +3,10 @@ require_dependency "liquor/application_controller"
 module Liquor
   class CompletionsController < ApplicationController
     def index
-      q         = params[:q]
-      klass     = q.classify.safe_constantize
-      drop      = q.classify.concat('Drop').safe_constantize
-      invokables = drop&.invokable_methods
+      q          = params[:q]
+      klass      = q.classify.safe_constantize
+      drop       = q.classify.concat('Drop').safe_constantize
+      invokables = drop&.invokable_methods || []
       # drop_relations = klass.reflect_on_all_associations.map(&:name).select {|n| invokables.include?(n.to_s) }
 
       render json: { q: q, completions: invokables }

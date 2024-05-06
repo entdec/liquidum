904f7fa7643ebdf5c8a57c2b536b623a13d5ba7b
Syed Waqas Ali Shah <s.waqas@rapidlogistics.nl>
2024-05-06 15:56:27 +0400

    add more satis updates to fix the errors

--- app/tables/liquidum/sandboxes_table.rb
+++ app/tables/liquidum/sandboxes_table.rb
@@ -1,19 +1,17 @@
 module Liquidum
-  class SandboxesTable < ApplicationTable
-    definition do
-      model Sandbox
+  class SandboxesTable < ActionTable::ActionTable
+    model Liquidum::Sandbox
 
-      column(:description)
-      column(:template)
-      column(:created_at)
-      column(:updated_at)
+    column(:description)
+    column(:template)
+    column(:created_at)
+    column(:updated_at)
 
-      # filter(:context) { |value| where("context ilike :query", query: "%#{value}%") }
-      # filter(:template) { |value| where("template ilike :query", query: "%#{value}%") }
-      # filter(:description) { |value| where("description ilike :query", query: "%#{value}%") }
+    # filter(:context) { |value| where("context ilike :query", query: "%#{value}%") }
+    # filter(:template) { |value| where("template ilike :query", query: "%#{value}%") }
+    # filter(:description) { |value| where("description ilike :query", query: "%#{value}%") }
 
-      link { |sandbox| liquidum.edit_sandbox_path(sandbox) }
-    end
+    row_link { |sandbox| liquidum.edit_sandbox_path(sandbox) }
 
     private
 
